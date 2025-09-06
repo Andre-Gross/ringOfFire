c986f4938ac15a7a7eb506c7dc551e1c35c31715
Andre-Gross <andre.handy94@gmail.com>
2025-09-06 15:23:07 +0200

    feat(start-screen): add start-screen component with grey background

---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-screen.component',
+  imports: [],
+  templateUrl: './start-screen.component.html',
+  styleUrl: './start-screen.component.scss'
+})
+export class StartScreenComponent {
+
+}
