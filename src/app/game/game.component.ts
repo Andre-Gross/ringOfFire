b5333c16b9c88f4007249f06087d0f2088e634df
Andre-Gross <andre.handy94@gmail.com>
2025-09-18 18:02:32 +0300

    feat(game): add wooden tabletop background image with centered card stack

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-game.component',
-  imports: [],
+  selector: 'app-game',
+  imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
 
