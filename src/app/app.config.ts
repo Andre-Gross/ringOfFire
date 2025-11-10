ce890aacd16a6549d8d534eed4695f2e79f45556
Andre-Gross <andre.handy94@gmail.com>
2025-11-10 13:24:02 +0100

    build(deps): add firebase storage as backend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "ringoffire-98d45", appId: "1:687813522023:web:d50f735bbbacda2b8f9ac2", storageBucket: "ringoffire-98d45.firebasestorage.app", apiKey: "AIzaSyB0o2m-bfrXRXWdihpxeoentNUs-dgXOBE", authDomain: "ringoffire-98d45.firebaseapp.com", messagingSenderId: "687813522023", projectNumber: "687813522023", version: "2" })), provideFirestore(() => getFirestore())
   ]
 };
